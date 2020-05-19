60a284dcc99cefe4f1dceacb8cbd486d79f235a9
Miguel <miguelcordobaaranda@gmail.com>
2020-05-19 20:06:56 +0200

    Modificado

--- app.py
+++ app.py
@@ -2,24 +2,21 @@ import requests
 import os
 from flask import Flask,render_template,abort
 app=Flask(__name__)
-#URL BASE
-base="https://api.soccersapi.com/v2.2/"
-#Variable de Entorno key
-key=os.environ["key"]
-usuario=os.environ["usuario"]
-#Parametros
-parametros={'key':key,'user':usuario}
 
 #PAISES
 @app.route('/',methods=["GET","POST"])
 def inicio():
-    r=requests.get(base+'countries/?user=miguelcor.rrss&token=ebb05a4abb67af480efa2a8eadee6ecb&t=list',params=parametros)
+    #URL BASE
+    base="https://api.soccersapi.com/v2.2/"
+    #Variables de Entorno
+    key=os.environ["key"]
+    usuario=os.environ["usuario"]
+    #Parametros
+    parametros={'key':key,'user':usuario,'t':"list"}
+    r=requests.get(base+'countries/',params=parametros)
     if r.status_code==200:
         doc=r.json()
-        paises=[]
-        for datos in doc["data"]:
-            paises.append(datos)
-        return render_template("inicio.html",lista=paises,key=key,usuario=usuario)
+        return render_template("inicio.html",lista=doc,key=key,usuario=usuario)
     else:
        return "No ha podido ser"
 #Equipos
