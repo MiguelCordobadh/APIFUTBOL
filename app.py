df7235782f58ba77a2ed123cad9b4e208ae945a9
Miguel <miguelcordobaaranda@gmail.com>
2020-05-30 19:49:54 +0200

    inicio,paises,jugadores

--- app.py
+++ app.py
@@ -2,23 +2,44 @@ import requests
 import os
 from flask import Flask,render_template,abort
 app=Flask(__name__)
-
-#PAISES
+#URL BASE
+base="https://api.soccersapi.com/v2.2/"
+#Variables de Entorno
+key=os.environ["key"]
+usuario=os.environ["usuario"]
+#INICIO
 @app.route('/',methods=["GET","POST"])
 def inicio():
-    #URL BASE
-    base="https://api.soccersapi.com/v2.2/"
-    #Variables de Entorno
-    key=os.environ["key"]
-    usuario=os.environ["usuario"]
+    return render_template("inicio.html")
+
+#PAISES
+@app.route('/paises',methods=["GET","POST"])
+def paises():
     #Parametros
-    parametros={'key':key,'user':usuario,'t':"list"}
+    parametros={'token':key,'user':usuario,'t':"list"}
     r=requests.get(base+'countries/',params=parametros)
     if r.status_code==200:
         doc=r.json()
-        return render_template("inicio.html",lista=doc,key=key,usuario=usuario)
+        continentes=[]
+        paises=[]
+        for dato in doc["data"]:
+            paises.append(dato["name"])
+            if dato["continent"] not in continentes:
+                continentes.append(dato["continent"])
+        return render_template("paises.html",lista=datos,contientes=continentes,paises=paises)
     else:
        return "No ha podido ser"
+
+@app.route('/jugadores/<id>',methods=["GET","POST"])
+def jugadores(id):
+    parametros={'token':key,'user':usuario,'t':"list",'country_id':id}
+    r=requests.get(base+'countries/',params=parametros)
+    if r.status_code==200:
+        doc=r.json()
+
+
+
+
 #Equipos
 #https://api.soccersapi.com/v2.2/teams/?user=miguelcor.rrss&token=ebb05a4abb67af480efa2a8eadee6ecb&t=list&country_id=3
 
